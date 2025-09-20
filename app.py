d7432d4144f67ea797b2ac3003fbbb0384b9eb6e
sumedh <sumedhp0424@gmail.com>
2025-09-20 23:11:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'dedication,determination,and consistent is the key of success ,'
+    return 'dedication,determination,consistent and decipline is the key of success ,'
 
 @app.route('/health')
 def health():
